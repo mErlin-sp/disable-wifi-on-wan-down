4781b739feb696264a8b2766db1aafc32431c8ae
Oleksandr <merlin.ua.1@gmail.com>
2023-01-28 01:02:28 +0200

    Add -s key to logger calls

--- disable_wifi_on_wan_down.sh
+++ disable_wifi_on_wan_down.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-echo "disable_wifi_on_wan_down script start"
+logger -s -t disable_wifi_on_wan_down "disable_wifi_on_wan_down script start"
 
 gateway="192.168.1.1"
 fail_counter=0
 
 wifi_up() {
   if [ "$(wifi status)" = "*\"up\": true*" ]; then
-    echo "wifi up"
+    logger -s -t disable_wifi_on_wan_down "wifi up"
     return 0
   else
-    echo "wifi down"
+    logger -s -t disable_wifi_on_wan_down "wifi down"
     return 1
   fi
 }
@@ -18,22 +18,22 @@ wifi_up() {
 while true; do
   if [ "$(ping -c 5 -q $gateway)" = "*0 packets received*" ]; then
     fail_counter=$((fail_counter + 1))
-    logger -t disable_wifi_on_wan_down "ifdown / fail_counter: $fail_counter"
+    logger -s -t disable_wifi_on_wan_down "ifdown / fail_counter: $fail_counter"
 
     if [ $fail_counter -gt 3 ]; then
-      logger -t disable_wifi_on_wan_down "ifdown"
+      logger -s -t disable_wifi_on_wan_down "ifdown"
       if wifi_up; then
-        logger -t disable_wifi_on_wan_down "disabling wifi"
+        logger -s -t disable_wifi_on_wan_down "disabling wifi"
         wifi down
         sleep 30s
       fi
       fail_counter=0
     fi
   else
-    logger -t disable_wifi_on_wan_down "ifup"
+    logger -s -t disable_wifi_on_wan_down "ifup"
 
     if ! wifi_up; then
-      logger -t disable_wifi_on_wan_down "enabling wifi"
+      logger -s -t disable_wifi_on_wan_down "enabling wifi"
       wifi up
       sleep 30s
     fi
